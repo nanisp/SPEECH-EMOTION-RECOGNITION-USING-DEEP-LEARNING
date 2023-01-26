6bdf5b32895a639c65371007b34688031fc36e2a
Pendyala Dhanalaxmi <100785663+nanisp@users.noreply.github.com>
2023-01-26 23:52:23 +0530

    Update serapp.py

--- serapp.py
+++ serapp.py
@@ -27,7 +27,7 @@ def get_file_content_as_string(path):
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
     
-@st.cache(show_spinner=False)
+@st.cache(show_spinner=True)
 def load_model():
     model=tf.keras.models.load_model('mymodel.h5')
     
